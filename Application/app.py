9af662a103856c341b3a5874211a02d6bf9a3b5c
lognorman20 <logannormanss@gmail.com>
2020-09-21 16:44:22 -0600

    updated for checking

--- Application/app.py
+++ Application/app.py
@@ -7,6 +7,7 @@ from flask import (
     render_template,
     redirect,
     render_template_string,
+    url_for,
 )
 import pickle
 import cgi
@@ -14,11 +15,11 @@ import jinja2
 import os
 
 
-app = Flask("Sneaker Price Predictor", template_folder={"templates"})
+app = Flask("Sneaker Price Predictor")
 model = pickle.load(open("/Users/logno/Documents/Home/BAF1/model.pkl", "rb"))
 
 
-@app.route("/", methods=["GET"])
+@app.route("/")
 def home():
     # Main page. The line below throws an error and idk why
     return render_template("index.html")
